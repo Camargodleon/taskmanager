f65d6afaa90e97e96b135d24720b492630ef0e19
Leonardo <leonardocccamargo@hotmail.com>
2020-03-09 11:42:38 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TarefaRoutes } from './tarefas/shared';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  ...TarefaRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
